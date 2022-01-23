be39cc442d5f1009d48b85bd8463dc4f2f5657bb
Will Medrano <will.s.medrano@gmail.com>
2022-01-23 15:34:24 -0800

    Implement GetPlugins.

--- loquat-server/src/main.rs
+++ loquat-server/src/main.rs
@@ -27,11 +27,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-struct LoquatServiceImpl {}
+struct LoquatServiceImpl {
+    lv2_world: livi::World,
+}
 
 impl LoquatServiceImpl {
     pub fn new() -> Self {
-        LoquatServiceImpl {}
+        let mut lv2_world = livi::World::new();
+        lv2_world.initialize_block_length(1, 8192).unwrap();
+        LoquatServiceImpl { lv2_world }
     }
 }
 
@@ -41,7 +45,15 @@ impl loquat_proto::loquat_server::Loquat for LoquatServiceImpl {
         &self,
         _: tonic::Request<loquat_proto::GetPluginsRequest>,
     ) -> Result<tonic::Response<loquat_proto::GetPluginsResponse>, tonic::Status> {
-        let plugins = vec![];
+        let plugins = self
+            .lv2_world
+            .iter_plugins()
+            .map(|p| loquat_proto::Plugin {
+                id: format!("lv2{}", p.uri()),
+                name: p.name(),
+                format: loquat_proto::plugin::Format::Lv2.into(),
+            })
+            .collect();
         Ok(tonic::Response::new(loquat_proto::GetPluginsResponse {
             plugins,
         }))
